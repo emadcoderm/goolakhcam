aad08c0ee173f047c8a1179f4d4aa62000819fb9
emadcoderm <sismarket.agent@gmail.com>
2025-10-31 01:40:45 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyA3eECZTkmItpQItkyCDkOrkYSfNl6FQHk),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyA3eECZTkmItpQItkyCDkOrkYSfNl6FQHk)
       },
       resolve: {
         alias: {
