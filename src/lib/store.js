3014ac073a8465895982cdb5750bf875a61c274e
emadcoderm <sismarket.agent@gmail.com>
2025-10-31 01:19:34 +0330

    feat: Remove unused immer import
    The 'immer' import was not being used in the store.js file and has been removed to clean up the code.

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,7 +2,6 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
 */
-import 'immer'
 import {create} from 'zustand'
 import {immer} from 'zustand/middleware/immer'
 import {createSelectorFunctions} from 'auto-zustand-selectors-hook'
